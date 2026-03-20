9478b2db3047c4e7fcf25a0ed912c3b4fddcf1fa
joseathael00 <joseathael00@gmail.com>
2026-03-20 16:29:49 -0300

    page

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ export default function App() {
 
   // Função que envia a url pra api
   async function sendUrl() {
-    // Requisição de método post que envia a url para api, que responderá a imagem do qrcode
     const response = await fetch("http://127.0.0.1:8000", {
       method: "POST",
       headers: {
@@ -42,15 +41,16 @@ export default function App() {
 
   return (
     <main className="flex flex-col items-center justify-center gap-2 w-screen h-screen bg-[#1a1c1e]">
-      <h1 className="text-white ">
+      <h1 className="text-white">
         Gerador de <span className="text-[#3A629D]">Qr</span>Code
       </h1>
 
       <div
-        className="grid grid-cols-1 grid-rows-[0.5fr_1fr] lg:grid-cols-2 lg:grid-rows-1 center 
-      w-[85dvw] h-[80dvh] md:w-[55dvw] lg:w-[80dvw] lg:h-[85dvh] xl:w-[60dvw] rounded-2xl bg-[#26292D] shadow-marine-blue relative"
+        className="grid grid-cols-1 grid-rows-[0.5fr_1fr] lg:grid-cols-2 lg:grid-rows-1
+        w-[85dvw] h-[80dvh] md:w-[55dvw] lg:w-[80dvw] lg:h-[85dvh] xl:w-[60dvw]
+        rounded-2xl bg-[#26292D] relative"
       >
-        <div className="flex flex-col justify-center place-items-center gap-10 w-[90%] h-[90%]  ">
+        <div className="flex flex-col justify-center place-items-center gap-10 w-full h-full">
           <input
             type="text"
             name="Input de Url"
@@ -59,11 +59,11 @@ export default function App() {
             // Adiciona o valor do input no state Url sempre que ele é mudado
             onChange={(e) => SetUrl(e.target.value)}
             placeholder="Cole aqui a url"
-            className="flex px-3 top-3 w-[90%] h-13 md:w-[70%] lg:w-[80%] xl:w-[80%] 2xl:h-[10%] border-3
-             border-solid border-[#606060] bg-[#5d5f62] outline-none"
+            className="px-3 w-[90%] h-12 md:w-[70%] lg:w-[80%] xl:w-[80%] border-2
+             border-solid border-[#606060] bg-[#5d5f62] outline-none rounded-lg"
           />
           <button
-            className="w-[70%] md:w-[50%] lg:w-[50%] xl:w-[45%] 2xl:h-[8%] hover:outline-none bg-[#1a1a1a]"
+            className="w-[70%] md:w-[50%] lg:w-[50%] xl:w-[45%] hover:outline-none bg-[#1a1a1a] rounded-lg py-2"
             onClick={sendUrl}
           >
             Gerar QrCode
@@ -73,26 +73,28 @@ export default function App() {
         <div
           // Adiciona a imagem do qrcode como background
           style={{ backgroundImage: `url(${qrCode})` }}
-          className="flex flex-col items-center justify-center w-full h-full md:h-70 md:w-70 lg:w-90 lg:h-90 xl:w-90 xl:h-90 
-        2xl:h-160 2xl:w-160 rounded-2xl bg-dark text-center shadow-marine-blue bg-contain bg-no-repeat bg-center "
+          className="flex flex-col items-center justify-center w-full h-full
+          rounded-2xl bg-[#0d0d0d] text-center bg-contain bg-no-repeat bg-center"
         >
           {/* Esses dois textos só aparecem se o qrcode ainda não foi gerado */}
           {!qrCode && (
-            <h2 className="font-semibold text-white 2xl:text-3xl ">
+            <h2 className="font-semibold text-white">
               Aguardando dados
             </h2>
           )}
           {!qrCode && (
-            <p className="text-base text-gray-400 2xl:text-2xl ">{subtitle}</p>
+            <p className="text-base text-gray-400">{subtitle}</p>
           )}
         </div>
-        {/* Esses botões só aparece quando o qrCode já foi gerado */}
+
+        {/* Esses botões só aparecem quando o qrCode já foi gerado */}
         {qrCode && (
           <button
-            className="h-12 w-12 absolute bottom-11 right-1/25 bg-cover bg-[#1a1a1a] "
+            style={{ right: '4%' }}
+            className="h-12 w-12 absolute bottom-10 bg-cover bg-[#1a1a1a] rounded-lg"
             id="download"
             onClick={() => {
-              // Função anônima de donwload quando o botão de download é apertado
+              // Função anônima de download quando o botão de download é apertado
               const link = document.createElement("a");
               link.href = qrCode;
               link.download = "qrcode.png";
@@ -103,7 +105,7 @@ export default function App() {
 
         {qrCode && (
           <button
-            className="h-10 w-33 absolute top-12 right-7 border-2 border-solid border-[#020202] bg-[#1a1a1a]"
+            className="h-10 w-32 absolute top-12 right-7 border-2 border-solid border-[#020202] bg-[#1a1a1a] rounded-lg"
             id="clear"
             onClick={() => {
               setQrCode("");
