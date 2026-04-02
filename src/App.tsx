f0592764a8359ca337ba7487801227a7aef2ec72
joseathael00 <joseathael00@gmail.com>
2026-04-02 16:13:57 -0300

    Adicionado o qrcode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import "./App.css";
+import { QRCodeSVG } from "qrcode.react";
 import { useEffect, useState } from "react";
 
+function QrCode({ url }: { url: string }) {
+  return url ? (
+    <QRCodeSVG
+      id="qr-canvas"
+      value={url}
+      size={256}
+      bgColor="#ffffff"
+      fgColor="#000000"
+      className="h-full w-full border-5 border-solid border-white rounded-2xl"
+      level="H"
+    />
+  ) : null;
+}
+
 export default function App() {
-  const [url, setUrl] = useState("");
-  console.log(url);
-  const [windowWidht, setWindowWidth] = useState(0);
+  function handleDownload() {
+    const svg = document.getElementById("qr-canvas");
+    if (!svg) return;
+
+    const svgData = new XMLSerializer().serializeToString(svg);
+
+    const svgBlob = new Blob([svgData], {
+      type: "image/svg+xml;charset=utf-8",
+    });
+    const url = URL.createObjectURL(svgBlob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "qrcode.svg";
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }
+  const [inputTextContent, setInputTextContent] = useState("");
+  const [generatedUrl, setGeneratedUrl] = useState("");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   // * UseEffect que guarda a largura da página sempre que o evento 'resize' é chamado
   useEffect(() => {
@@ -18,43 +53,72 @@ export default function App() {
   }, []);
 
   return (
-    <main className="flex-center h-[95dvh] w-[95dvw] gap-2  p-10">
+    <main className="flex-center h-[95dvh] w-[95dvw] gap-2 p-10">
       <h1 className="text-3xl 2xl:text-6xl">
         Gerador de <span className="text-[#3A629D]">Qr</span>Code
       </h1>
 
       <div
         className="grid grid-cols-1 grid-rows-2 place-items-center h-8/9 w-8/9 sm:grid-cols-2 sm:grid-rows-1  min-[425px]:w-90 sm:w-10/12
-       lg:w-230 2xl:w-10/12  bg-[#26292D] rounded shadow-marine-blue"
+       lg:w-230 2xl:w-10/12  bg-[#26292D] rounded shadow-marine-blue "
       >
-        <div className="flex-center gap-5 ">
+        <div className="flex-center gap-5 relative">
+          {generatedUrl ? (
+            <>
+              <button
+                onClick={() => {
+                  setGeneratedUrl("");
+                  setInputTextContent("");
+                }}
+                className="absolute bg-dark h-15 w-30 shadow-marine-blue bottom-75 right-80"
+              >
+                Limpar
+              </button>
+
+              <button
+                onClick={() => handleDownload()}
+                className="absolute bg-dark h-15 w-40 shadow-marine-blue top-75 left-75"
+              >
+                Baixar imagem
+              </button>
+            </>
+          ) : null}
+
           <input
             type="text"
+            value={inputTextContent}
             placeholder="Cole sua URL aqui"
-            onChange={(e) => setUrl(e.target.value)} // * Muda o valor do input sempre que algo novo é digitado dentro dele
+            onChange={(e) => setInputTextContent(e.target.value)} // * Muda o valor do input sempre que algo novo é digitado dentro dele
             className="h-10 min-w-15 2xl:h-15 2xl:w-80 2xl:text-[1.2rem] px-2! text-neutral-200 bg-[#3A3F44]
        border border-solid border-[#171616] rounded-sm outline-0"
           />
 
-          <button className="h-10 w-60 2xl:h-15 2xl:w-80  bg-dark shadow-marine-blue">
+          <button
+            onClick={() => setGeneratedUrl(inputTextContent)}
+            className="h-10 w-60 2xl:h-15 2xl:w-80 bg-dark shadow-marine-blue"
+          >
             <p className="2xl:text-[1.2rem]">Gerar</p>
           </button>
         </div>
 
         <div
           className="flex-center w-[90%] h-[90%] min-[325px]:w-65 min-[325px]:h-65 sm:h-75 sm:w-70 lg:h-90 lg:w-90 2xl:w-150
-         2xl:h-150 bg-dark rounded-2xl shadow-marine-blue "
+         2xl:h-150 rounded-2xl shadow-marine-blue bg-dark "
         >
-          <p className="text-[20px] 2xl:text-3xl text-white ">
-            Aguardando dados...
-          </p>
-
-          {/* Exibe a primeira mensagem caso a condição 'windowWidth < 768' seja verdadeira, caso seja falsa, exibe a segunda */}
-          <p className="text-[15px] 2xl:text-2xl text-[#B1AEAE]">
-            {windowWidht < 645
-              ? "Preencha os campos acima"
-              : "Preencha os campos ao lado"}
-          </p>
+          {generatedUrl ? (
+            <QrCode url={generatedUrl} />
+          ) : (
+            <>
+              <p className="text-[20px] 2xl:text-3xl text-white ">
+                Aguardando dados...
+              </p>
+              <p className="text-[15px] 2xl:text-2xl text-[#B1AEAE]">
+                {windowWidth < 645
+                  ? "Preencha os campos acima"
+                  : "Preencha os campos ao lado"}
+              </p>
+            </>
+          )}
         </div>
       </div>
     </main>
